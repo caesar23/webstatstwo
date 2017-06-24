873d4152d9e5cce1471abf45a2a3892ecea76a12
Kamil Wozniak <kamwo@mailbox.org>
2017-06-24 15:44:46 +0200

    Renamed interface. Removed "public" scope from interface, because it is by default public.

--- src/main/java/ro/ucv/inf/service/GeoIpLocationService.java
+++ src/main/java/ro/ucv/inf/service/GeoIpLocationService.java
@@ -1,46 +1,13 @@
 package ro.ucv.inf.service;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.InetAddress;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.stereotype.Service;
-
-import com.maxmind.db.Reader.FileMode;
-import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
-import com.maxmind.geoip2.model.CityResponse;
 
 import ro.ucv.inf.model.GeoIpLocation;
 
-@Service("geolocationservice")
-public class GeoIpLocationService implements IGeoIpLocationService {
+public interface GeoIpLocationService {
 
-	private DatabaseReader dbReader;
+	GeoIpLocation getLocation(String ip) throws IOException, GeoIp2Exception;
 
-	@Autowired
-	public GeoIpLocationService(ResourceLoader resourceLoader) {
-		Resource resource = resourceLoader.getResource("classpath:localdb/GeoLite2-City.mmdb");
-		InputStream dbAsStream;
-		try {
-			dbAsStream = resource.getInputStream();
-			dbReader = new DatabaseReader.Builder(dbAsStream).fileMode(FileMode.MEMORY).build();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public GeoIpLocation getLocation(String ip) throws IOException, GeoIp2Exception {
-		InetAddress ipAddress = InetAddress.getByName(ip);
-		CityResponse response = dbReader.city(ipAddress);
-
-		String cityName = response.getCity().getName();
-		String latitude = response.getLocation().getLatitude().toString();
-		String longitude = response.getLocation().getLongitude().toString();
-		return new GeoIpLocation(ip, cityName, latitude, longitude);
-	}
-	
 }
